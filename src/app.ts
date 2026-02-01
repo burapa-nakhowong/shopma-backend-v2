2e0efc78e325e981cd40a13beac17f51c9abe734
burapa-nakhowong <burapa.nakhowong@gmail.com>
2026-02-01 23:05:56 +0700

    debug - endpoint in vercel

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,31 @@
 import express from "express";
 import cors from "cors";
-import routes from './routes/index.js';
+import routes from "./routes/index.js";
 import prisma from "./config/db.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
-
 import { requestLogger } from "./middlewares/performance.middleware.js";
 
-export const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
+// ⭐ สำคัญมาก
 app.use(routes);
 
 app.get("/", (_req, res) => {
     res.json({ status: "ok" });
 });
 
-// test
-app.get('/db', async (req, res) => {
+app.get("/db", async (_req, res) => {
     try {
-        // query เช็คการเชื่อมต่อ
         await prisma.$queryRaw`SELECT 1`;
-        res.json({
-            status: 'ok',
-            db: 'connected'
-        });
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            status: 'error',
-            db: 'disconnected'
-        });
+        res.json({ status: "ok", db: "connected" });
+    } catch {
+        res.status(500).json({ status: "error", db: "disconnected" });
     }
 });
-
 
 app.use(errorHandler);
 
