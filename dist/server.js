8cd3ace79ae0b8931458f684d64512c8e59f6e9a
burapa-nakhowong <burapa.nakhowong@gmail.com>
2026-02-02 14:49:07 +0700

    debug error vercel  manage package

--- dist/server.js
+++ dist/server.js
@@ -1,5 +1,13 @@
 import app from './app.js';
+
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+// เช็คว่าไม่ได้รันบน Vercel (โหมด Development) ค่อยสั่ง listen
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+// ต้อง export default app ออกไปด้วย
+export default app;
