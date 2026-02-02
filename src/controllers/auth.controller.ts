383bf28d3cd812a43547e3793f246bfb6826d2cd
burapa-nakhowong <burapa.nakhowong@gmail.com>
2026-02-02 15:25:45 +0700

    debug - cors cookie

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -38,7 +38,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
         const accessToken = await authService.generateAccessToken(result.id, result.role);
         res.cookie("access_token", accessToken, {
             httpOnly: true,                                 // JS อ่านไม่ได้ (กัน XSS)
-            secure: `${process.env.NODE_ENV} === "production"`,  // https เท่านั้น
+            secure: process.env.NODE_ENV === "production",  // https เท่านั้น
             sameSite: "none",                                // ป้องกัน CSRF พื้นฐาน
             maxAge: 60 * 60 * 1000,                         // 1 ชั่วโมง
         });
